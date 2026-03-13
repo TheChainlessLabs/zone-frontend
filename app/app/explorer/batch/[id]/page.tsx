cb0c99d1727ec094dc88a939a85bf9f162cb48d2
Brian Seong <brianseong.dev@gmail.com>
2026-03-13 13:25:25 +0900

    fix: responsive padding on all detail and settings pages
    Change p-6 to p-4 md:p-6 on page wrappers for proper mobile spacing.

--- app/app/explorer/batch/[id]/page.tsx
+++ app/app/explorer/batch/[id]/page.tsx
@@ -12,7 +12,7 @@ export default async function BatchPage({ params }: BatchPageProps) {
     <div className="flex flex-col min-h-screen">
       <Navbar />
 
-      <div className="flex-1 p-6 max-w-[1200px] mx-auto w-full">
+      <div className="flex-1 p-4 md:p-6 max-w-[1200px] mx-auto w-full">
         <BatchDetail batchId={id} />
       </div>
     </div>
